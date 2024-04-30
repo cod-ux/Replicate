523d73af6bdf265231c30fbc28f81e91ddc3d7dd
Surya Govindasamy Ganesan <suryaganesan925@gmail.com>
2024-04-30 13:08:08 +0100

    yes

---
+++ langgraph_chat_agent_executor.py
@@ -0,0 +1,79 @@
+import toml
+import os
+import phoenix as px
+from phoenix.trace.langchain import LangChainInstrumentor
+
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langgraph.prebuilt import ToolExecutor
+from langchain_openai import ChatOpenAI
+from langchain.tools.render import format_tool_to_openai_function
+
+from typing import TypedDict, Annotated, Sequence
+import operator
+from langchain_core.messages import BaseMessage
+
+from langgraph.prebuilt import ToolInvocation
+from langchain_core.messages import FunctionMessage
+import json
+
+secrets = "/Users/suryaganesan/Documents/GitHub/Replicate/secrets.toml"
+github_secrets = "secrets.toml"
+
+os.environ["OPENAI_API_KEY"] = toml.load(github_secrets)["OPENAI_API_KEY"]
+os.environ["TAVILY_API_KEY"] = toml.load(github_secrets)["TAVILY_API_KEY"]
+
+
+
+session = px.launch_app()
+LangChainInstrumentor().instrument()
+
+# Setup tools
+
+tools = [TavilySearchResults(max_results=1)]
+tool_exec = ToolExecutor(tools)
+
+#Setup model
+
+model = ChatOpenAI(model="gpt-3.5-turbo", temperature=0.7, streaming=True)
+
+functions = [format_tool_to_openai_function(t) for t in tools]
+model = model.bind_functions(functions)
+
+# Define agent state
+
+class AgentState(TypedDict):
+    messages: Annotated[Sequence[BaseMessage], operator.add]
+
+# Define nodes
+
+def should_continue(state):
+    messages = state["messages"]
+    last_message = messages[-1]
+
+    if "function_call" not in last_message.additional_kwargs:
+        return "end"
+
+    else:
+        return "continue"
+
+def call_model(state):
+    messages = state["messages"]
+    response = model.invoke(messages)
+    return {"messages": [response]}
+
+
+def call_tool(state):
+    messages = state["messages"]
+    last_message = messages[-1]
+    action = ToolInvocation(
+        tool = last_message.additional_kwargs["function"]["name"],
+        tool_input = json.loads(
+            last_message.additional_kwargs["function"]["arguments"]
+            )
+    )
+
+    response = tool_exec.invoke(action)
+    function_message = FunctionMessage(content=str(response), name=action.tool)
+    return {"messages": function_message}
+
+    
